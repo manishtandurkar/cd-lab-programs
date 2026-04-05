02dcb7619b42fd8680ab0668ad9c9013d2eb9d97
Manish Tandurkar <139057751+manishtandurkar@users.noreply.github.com>
2026-04-05 18:55:21 +0530

    Program 3 and 4

---
+++ 4/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// This is a single-line comment
+int main() {
+    int a = 10; // variable declaration
+
+    /* This is a 
+       multi-line comment */
+
+    printf("Hello World\n");
+
+    /* Another comment
+       spanning multiple
+       lines */
+
+    int b = 20;
+
+    // Final single-line comment
+
+    return 0;
+}
